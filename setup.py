797fe1bce701490b361ce980c98c227c7aca6a7f
Michael Mi <guocuimi@gmail.com>
2020-02-27 01:26:18 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
 from setuptools import find_packages
 from setuptools import setup
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    lic = f.read()
+
+with open('requirements.txt') as f:
+    reqs = list(f.read().strip().split('\n'))
+
 setup(
     name='minitf',
     version='0.2.2.1',
     description='Simplified version of Tensorflow for learning purposes.',
+    long_description=readme,
     author='Michael Mi',
     author_email='guocuimi@gmail.com',
-    install_requires=['numpy>=1.9.1', 'matplotlib>=3.1.1'],
+    url='https://github.com/guocuimi/minitf',
+    license=lic,
+    install_requires=reqs,
+    packages=find_packages(exclude=('tests')),
+    package_data={'': ['README.md', 'LICENSE']},
     keywords=['Deep learning', 'Tensorflow', 'Autodiff', 'Backpropagation',
               'Gradients', 'Neural networks', 'Python', 'Numpy'],
-    url='https://guocuimi.github.io/minitf/',
-    license='MIT',
-    classifiers=['Development Status :: 1 - Planning',
+    classifiers=['Development Status :: 2 - Pre-Alpha',
                  'Intended Audience :: Developers',
                  'Intended Audience :: Education',
                  'Intended Audience :: Science/Research',
-                 'License :: OSI Approved :: MIT License',
-                 'Programming Language :: Python :: 3.7'],
-    packages=find_packages(),
+                 'License :: OSI Approved :: MIT License'],
 )
